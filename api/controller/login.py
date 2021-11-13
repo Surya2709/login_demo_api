6dabe1d6e7c3269a2a7410932a9dd9dabb671c3f
surya <surya@divum.in>
2021-11-13 18:53:23 +0530

    working version

--- api/controller/login.py
+++ api/controller/login.py
@@ -10,9 +10,7 @@ import traceback
 
 
 def encrypt_pass(password):
-
     key = str(Config.SECRET_KEY)
-    
     encrypted_pass = cryptocode.encrypt(password,key)
     return encrypted_pass
 
@@ -22,15 +20,11 @@ def registration(body):
     try:
     
         user_name = body.get('user_name',None)
-
         if session.query(user_reg).filter(user_reg.user_name==user_name).first():
             resp = { "resp" : "user name already exist"}
             return 400,resp
-
         user_pass =  body.get("user_pass",None)
-
         pass_ = encrypt_pass(str(user_pass))
-
         obj = { "user_id" : uuid.uuid4(),"user_name" : "surya", "user_pass" : pass_ }
         u = user_reg(**obj)
         session.add(u)
@@ -47,16 +41,13 @@ def login(body):
 
     user_name_ = body.get('user_name',None)
     user_pass =  body.get("user_pass",None)
-
     user_data = session.query(user_reg).filter(user_reg.user_name==user_name_).first()
     
     if not user_data:
         resp = { "resp" : "user name does not exist"}
         return 400,resp
-
     print(user_data.user_pass)
     pass_ = decrypt(str(user_data.user_pass),str(Config.SECRET_KEY))
-
     if user_pass==pass_:
         resp = { "resp" : "success"}
         return 200,resp
